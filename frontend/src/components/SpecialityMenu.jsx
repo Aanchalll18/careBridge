00620c7b3a6fcf9854d5a700e9959d5d2e0a41c6
Rinky Kumari <113056646+Rinky73577@users.noreply.github.com>
2025-06-15 23:43:40 +0530

    commit

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const SpecialityMeny = () => {
+  return (
+    <div>
+      <Header/>
+      <SpecialityMenu />
+    </div>
+  )
+}
+
+export default SpecialityMeny
