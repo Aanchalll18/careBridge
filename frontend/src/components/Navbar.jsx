8c144914570621c9df1a9a15be1b63aa5ed1c270
Rinky Kumari <113056646+Rinky73577@users.noreply.github.com>
2025-06-15 15:04:22 +0530

    navbar-colors
    navbar alignment

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,25 +4,21 @@ import { assets } from '../assets/assets'
 const Navbar = () => {
   return (
     <div className='flex item-centre justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-      <img src={assets.logo} alt="" />
-      <ul>
+      <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink>
-            <li>HOME</li>
-            <hr>
-            </hr>
+            <li className='py-1'>HOME</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'></hr>
         </NavLink>
         <NavLink>
-            <li>ALL DOCTORS</li>
-            <hr>
-            </hr>
+            <li className='py-1'>ALL DOCTORS</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'></hr>
         </NavLink><NavLink>
-            <li>ABOUT</li>
-            <hr>
-            </hr>
+            <li className='py-1'>ABOUT</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'></hr>
         </NavLink><NavLink>
-            <li>CONTACT</li>
-            <hr>
-            </hr>
+            <li className='py-1'>CONTACT</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'></hr>
         </NavLink>
       </ul>
       <div>
