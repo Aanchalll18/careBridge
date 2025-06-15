a2b50008c333025824fe3a963e8194f7044c7528
Rinky Kumari <113056646+Rinky73577@users.noreply.github.com>
2025-06-15 23:38:01 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,17 @@ import MyAppointment from './pages/MyAppointment'
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:speciality' element={<Doctors />}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
         <Route path='/my-profile' element={<MyProfile/>}/>
         <Route path='/my-appointment' element={<MyAppointment/>}/>
+        <Route path='/appointment/:socId' element={<MyAppointment/>}/>
       </Routes>
     </div>
   )
