5cae3e34cd560ab951cde4ea035364a294d35578
Aanchallll18 <kumariaachal002@gmail.com>
2025-06-15 11:39:34 +0530

    commit

---
+++ frontend/src/pages/MyProfile.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import {assets} from '../assets/'
+
+const MyProfile = () => {
+     
+    const [userData,setUserData]=useState({
+        name:"Edward Vincent",
+        image:
+    })
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyProfile
