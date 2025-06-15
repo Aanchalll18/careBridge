98c9960300de01f3587d494fbfdb89d2556e1bac
Aanchallll18 <kumariaachal002@gmail.com>
2025-06-15 21:35:00 +0530

    updated models

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDb from './config/mongodb.js'
+import connectCloudinary from './config/clodniary.js'
 
 const app=express()
 const port=process.env.PORT || 4000
 connectDb()
+connectCloudinary()
 
 app.use(express.json())
 app.use(cors())
