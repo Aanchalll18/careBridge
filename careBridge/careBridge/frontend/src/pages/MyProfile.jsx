81cfc72beeea866aa38888cc3270d2cf26725296
Rinky Kumari <113056646+Rinky73577@users.noreply.github.com>
2025-06-15 13:28:46 +0530

    Add files via upload

---
+++ careBridge/careBridge/frontend/src/pages/MyProfile.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import {assets} from '../assets/assets'
+
+const MyProfile = () => {
+     
+    const [userData,setUserData]=useState({
+        name:"Edward Vincent",
+        image:assets.profile_pic,
+        email:'richardjames@gmail.com',
+        phone:'+1 123 456 7890',
+        address:{
+            line1:"57th Cross,Richmond",
+            line2:'Circle,Cruch Road,London'
+        },
+        gender:'Male',
+        dob:'2000-01-20'       
+    })
+
+    const [isEdit,setIsEdit]=useState(false)
+
+  return (
+    <div>
+      <img src={userData.image} alt="" />
+      {
+        isEdit 
+        ? <input type='text' 
+        value={userData.name}
+        onChange={e=>setUserData(prev=>({...prev,name:e.target.value}))}/>
+        : <p>{userData.name}</p>
+      }
+      <hr />
+      <div></div>
+    </div>
+  )
+}
+
+export default MyProfile
