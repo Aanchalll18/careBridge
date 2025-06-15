48ee045841e1846e6a2c2d349a63d3428cc3e42c
Aanchallll18 <kumariaachal002@gmail.com>
2025-06-15 14:59:21 +0530

    backend added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDb from './config/mongodb.js'
+
+const app=express()
+const port=process.env.PORT || 4000
+connectDb()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send('Api Working')
+})
+
+app.listen(port,()=>console.log("Server Started",port))
