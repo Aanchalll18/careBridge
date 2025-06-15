428f147d85e00462294fdd5a98581c14d79f4be9
Rinky Kumari <113056646+Rinky73577@users.noreply.github.com>
2025-06-16 00:08:13 +0530

    Update SpecialityMenu.jsx

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Header from './Header'
+import { specialityData } from '../assets/assets'
+import { Link } from 'react-router-dom'
 
-const SpecialityMeny = () => {
+const SpecialityMenu = () => {
   return (
-    <div>
-      <Header/>
-      <SpecialityMenu />
+    <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='speciality'>
+      <h1 className='text-3x1 font-medium'>Find by Speciality</h1>
+      <p>Simply browse through our extension list of trusted doctors, schedule your appointment hassle free.</p>
+      <div>
+          {specialityData.map((item,index)=>(
+            <Link key={index} to={'/doctor/${item.speciality'}>
+                <img src="item.image" alt="" />
+                <p>{item.speciality}</p>
+            </Link>
+          ))}
+      </div>
     </div>
   )
 }
 
-export default SpecialityMeny
+export default SpecialityMenu
