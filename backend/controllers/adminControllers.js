bc34b0eb0de90eb5e1397875859d68aedf55d8ea
Aanchallll18 <kumariaachal002@gmail.com>
2025-06-16 00:06:40 +0530

    commit

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -1,10 +1,30 @@
-
+import validator from 'validator'
+import bcrypt from 'bcrypt'
 
 const addDoctor=async(req,res)=>{
     try {
         const {name,email,password,speciality,degree,experience,about,fees,address}=req.body
 
         const imageFile=req.file
+
+        if(!name|| !email|| !password|| !speciality|| !degree|| !experience || !about|| !fees|| !address){
+             return res.json({
+                success:false,
+                message:"Missing Data"
+             })
+        }
+        if(!validator.isEmail(email)){
+            return res.json({
+                success:false,
+                message:"Please enter a valid email"
+            })
+        }
+        if(password.length<8){
+            return res.json({
+                success:false,
+                message:"Please enter a strong password"
+            })
+        }
         
     } catch (error) {
         
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import MyProfile from './pages/MyProfile'
 import MyAppointment from './pages/MyAppointment'
+import Home from './pages/Home'
 
 const App = () => {
   return (
